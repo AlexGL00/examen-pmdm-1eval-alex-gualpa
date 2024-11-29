a9d630e026ea0088689c58a04016a9d4c5a24c82
AlexGL00 <alexgualpa012@hotmail.com>
2024-11-29 14:19:06 +0100

    finalizado

---
+++ app/src/main/java/com/examalexgualpa/examenpmdm/fr/scroll/boton.java
@@ -0,0 +1,21 @@
+package com.examalexgualpa.examenpmdm.fr.scroll;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.examalexgualpa.examenpmdm.R;
+
+public class boton extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.id.fragment_boton.)
+    }
+}
